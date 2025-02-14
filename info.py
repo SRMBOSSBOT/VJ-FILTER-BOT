486fe41e7faa06ec9ef61d0025612f4fbd169cf4
SRMBOSSBOT <srmboss4@gmail.com>
2025-02-14 22:05:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -108,10 +108,10 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', '')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '')
-VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
+VERIFY = bool(environ.get('VERIFY', True))
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'https://omegalinks.in/st?api=5369d6f0ce9cfdc7bd8865dc770c8452df9458f3&url=yourdestinationlink.com')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '5369d6f0ce9cfdc7bd8865dc770c8452df9458f3')
+VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Verify_Movie')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
